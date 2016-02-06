f0a4767c53b94951f07097d459103ebf58ff8fd6
Mark Johnson <marknotgeorge90@hotmail.com>
2016-02-06 09:31:58 +0000

    Added InitialBearing and FinalBearing

--- BGExtensionsTestApp/UnitTest.cs
+++ BGExtensionsTestApp/UnitTest.cs
@@ -8,26 +8,60 @@ namespace BGExtensionsTestApp
     [TestClass]
     public class UnitTests
     {
-        [TestMethod]
-        public void TestGetDistanceTo()
+        public BasicGeoposition ArnosGrove;
+        public BasicGeoposition FiveKilometresEast;
+        private double errorPercent = 0.3 / 100;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            BasicGeoposition ArnosGrove = new BasicGeoposition()
+            ArnosGrove = new BasicGeoposition()
             {
                 Latitude = 51.6163,
                 Longitude = 0.1335
             };
 
-            BasicGeoposition FiveKilometresEast = new BasicGeoposition()
+            FiveKilometresEast = new BasicGeoposition()
             {
                 Latitude = 51.6163,
                 Longitude = 0.2059
             };
+        }
+
+        [TestMethod]
+        public void TestGetDistanceTo()
+        {
+            Initialize();
 
             var result = ArnosGrove.GetDistanceTo(FiveKilometresEast);
 
-            var errorDelta = 5000 * 0.003;
+            var errorDelta = 5000 * errorPercent;
 
             Assert.AreEqual(5000, result, errorDelta);
+        }
+
+        [TestMethod]
+        public void TestInitialBearing()
+        {
+            Initialize();
+
+            var result = ArnosGrove.InitialBearing(FiveKilometresEast);
+
+            var errorDelta = 90 * errorPercent;
+
+            Assert.AreEqual(result, 90, errorDelta);
+        }
+
+        [TestMethod]
+        public void TestFinalHeading()
+        {
+            Initialize();
+
+            var result = ArnosGrove.FinalBearing(FiveKilometresEast);
+
+            var errorDelta = 90.0284 * errorPercent;
+
+            Assert.AreEqual(result, 90, errorDelta);
         }
     }
 }
